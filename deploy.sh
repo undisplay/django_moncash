45cf127d72efaba3fd5b11fcbb854ecac3d220ea
Freedy Meritus <meritusfreedy@gmail.com>
2023-04-07 16:46:59 -0400

    fix deploy issue

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd django_moncash/
+
 python3 -m pip install -U wheel twine setuptools
-python3 ./django_moncash/setup.py sdist
-python3 ./django_moncash/setup.py bdist_wheel
-twine upload ./django_moncash/dist/*
+python3 setup.py sdist
+python3 setup.py bdist_wheel
+twine upload dist/*
--- django_moncash/setup.py
+++ django_moncash/setup.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
 if __name__ == "__main__":
-    import setuptools
-    setuptools.setup()
+
+    from setuptools import setup
+
+    # read the contents of your README file
+    from pathlib import Path
+    this_directory = Path(__file__).parent
+    long_description = (this_directory / "../README.md").read_text()
+
+    setup(
+        name='django_moncash',
+        long_description=long_description,
+        long_description_content_type='text/markdown'
+    )
